e668b578c40c822619b7b77cd00ed3683b93e4e8
huxin <188296677@qq.com>
2024-02-13 14:35:53 +0800

    modify20240213

--- AStar&Dijkstra/dijkstra2.ts
+++ AStar&Dijkstra/dijkstra2.ts
@@ -1,4 +1,7 @@
 //学习网上的dijkstra算法
+// https://patrickkarsh.medium.com/dijkstras-shortest-path-algorithm-in-javascript-1621556a3a15
+// 感觉该算法的效率很低
+
 
 let graph = {
     A: { B: 1, C: 4 },
@@ -7,14 +10,40 @@ let graph = {
     D: { B: 5, C: 1 }
 }
 
-function dijkstra2(graph, start, end?) {
-    let result: Map<string, number> = new Map(); //存储已获得最短距离的结点
 
-    
-    let distanceMap = initialDistance(graph, distanceMap);
+function dijkstra2(graph, start: string, end?: string) {
+    let distances = {};
 
-    // let currentNode = 
+    let visited = new Set();
 
-    let visited: Set<string> = new Set();    
+    let nodes = Object.keys(graph);
 
+    for (let node of nodes) {
+        distances[node] = Infinity;
+    }
+
+    distances[start] = 0;
+
+    while (nodes.length) {
+        nodes.sort((a,b) => distances[a]-distances[b]);
+        let closestNode = nodes.shift();
+
+        if (distances[closestNode] === Infinity) {
+            break;
+        }
+
+        visited.add(closestNode);
+
+        for (let neighbor in graph[closestNode]) {
+            if (!visited.has(neighbor)) {
+                let newDistance = distances[closestNode] + graph[closestNode][neighbor];
+            }
+
+            if (newDistance < distances[neighbor]) {
+                distances[neighbor] = newDistance;
+            }
+        }
+    }
+
+    return distances;
 }
